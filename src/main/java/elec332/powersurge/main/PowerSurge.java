923677949ae3cfdc4e80c839824132e7cb4babe7
Elec332 <arnout1998@gmail.com>
2015-03-12 15:30:02 +0100

    Commit baseMod

---
+++ src/main/java/elec332/powersurge/main/PowerSurge.java
@@ -0,0 +1,86 @@
+package elec332.powersurge.main;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import elec332.powersurge.init.BlockRegister;
+import elec332.powersurge.init.CommandRegister;
+import elec332.powersurge.init.ItemRegister;
+import elec332.core.helper.FileHelper;
+import elec332.core.helper.MCModInfo;
+import elec332.core.modBaseUtils.ModBase;
+import elec332.core.modBaseUtils.modInfo;
+import elec332.powersurge.proxies.CommonProxy;
+
+import java.io.File;
+
+/**
+ * Created by Elec332 on 24-2-2015.
+ */
+@Mod(modid = PowerSurge.ModID, name = PowerSurge.ModName, dependencies = modInfo.DEPENDENCIES+"@[#ELECCORE_VER#,)",
+        acceptedMinecraftVersions = modInfo.ACCEPTEDMCVERSIONS, useMetadata = true, canBeDeactivated = true)
+public class PowerSurge extends ModBase {
+
+    public static final String ModName = "Power Surge"; //Human readable name
+    public static final String ModID = "PowerSurge";  //modid
+
+    @SidedProxy(clientSide = "elec332.powersurge.proxies.ClientProxy", serverSide = "elec332.powersurge.proxies.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance(ModID)
+    public static PowerSurge instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        this.cfg = FileHelper.getConfigFileElec(event);
+        loadConfiguration();
+        ItemRegister.instance.preInit(event);
+        BlockRegister.instance.preInit(event);
+        //setting up mod stuff
+
+        loadConfiguration();
+        MCModInfo.CreateMCModInfo(event, "Created by Elec332",
+                "Power Surge -insert description-",
+                "Loading URL...", "logo.png",
+                new String[]{"Elec332"});
+        notifyEvent(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        loadConfiguration();
+        ItemRegister.instance.init(event);
+        BlockRegister.instance.init(event);
+        //register items/blocks
+
+        notifyEvent(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+        loadConfiguration();
+        //Mod compat stuff
+
+        notifyEvent(event);
+    }
+
+    @Mod.EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        CommandRegister.instance.init(event);
+    }
+
+    File cfg;
+
+    @Override
+    public File configFile() {
+        return cfg;
+    }
+
+    @Override
+    public String modID(){
+        return ModID;
+    }
+}
