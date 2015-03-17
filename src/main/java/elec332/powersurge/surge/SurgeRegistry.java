6a767c521e6a5d71b00ad0008cd7d8eba073d3f1
Elec332 <arnout1998@gmail.com>
2015-03-17 16:25:17 +0100

    Require ability name to be lowercased

--- src/main/java/elec332/powersurge/surge/SurgeRegistry.java
+++ src/main/java/elec332/powersurge/surge/SurgeRegistry.java
@@ -1,6 +1,9 @@
 package elec332.powersurge.surge;
 
 import elec332.powersurge.api.IAbility;
+import elec332.powersurge.items.AbilityItems;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.util.ReportedException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,7 +16,14 @@ public class SurgeRegistry {
     private static HashMap<String, IAbility> registry = new HashMap<String, IAbility>();
 
     public static void registerAbility(IAbility ability){
-        registry.put(ability.getName(), ability);
+        if (ability.getName().equals(ability.getName().toLowerCase())) {
+            registry.put(ability.getName(), ability);
+            new AbilityItems(ability.getName());
+        } else {
+            CrashReport report = new CrashReport("Registering Ability", new Exception());
+            report.makeCategory("Encountered an error white trying to register ability: "+ability.getName()+"  The ability name isn't lowercase!");
+            throw new ReportedException(report);
+        }
     }
 
     public static IAbility getAbilityForName(String i){
