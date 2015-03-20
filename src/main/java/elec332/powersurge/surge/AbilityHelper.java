5cdc358fdaf6fe27e882ff498b89b126f658c58d
Elec332 <arnout1998@gmail.com>
2015-03-20 16:27:59 +0100

    Added AbilityHelper

---
+++ src/main/java/elec332/powersurge/surge/AbilityHelper.java
@@ -0,0 +1,18 @@
+package elec332.powersurge.surge;
+
+import elec332.powersurge.api.IAbility;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+/**
+ * Created by Elec332 on 20-3-2015.
+ */
+public class AbilityHelper {
+
+    public static boolean doesPlayerHaveAbility(EntityPlayerMP player, IAbility ability){
+        return areAbilitiesEqual(SurgeData.get(player).getSelectedAbility(), ability) && SurgeData.get(player).isAbilityActive();
+    }
+
+    public static boolean areAbilitiesEqual(IAbility ability1, IAbility ability2){
+        return !(ability1 == null || ability2 == null) && ability1.getName().equals(ability2.getName());
+    }
+}
